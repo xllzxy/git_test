7ca2423c05ad59079cb15333af515de1ef4f7d70
xielinling <404667132@qq.com>
2021-01-06 19:46:50 +0800

    添加了一个方法

--- git-test-project/src/com/xll/git/FirstClass.java
+++ git-test-project/src/com/xll/git/FirstClass.java
@@ -8,4 +8,10 @@ public class FirstClass {
         System.out.println("hello git project");
         System.out.println("hello git project");
     }
+
+    public void test(){
+        int i=10;
+        i=20;
+    }
+
 }
