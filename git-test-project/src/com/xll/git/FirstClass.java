a930a5fa0eb15f1212f0654e0a48b2f95a21ce0e
xielinling <404667132@qq.com>
2021-01-06 19:44:21 +0800

    本地工程添加到git

---
+++ git-test-project/src/com/xll/git/FirstClass.java
@@ -0,0 +1,11 @@
+package com.xll.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
